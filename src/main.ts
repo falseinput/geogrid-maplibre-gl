b760d59fe1c8698122f7b694e2263b13ca6c6d10
Zbigniew Matysek <matysek.zbigniew@gmail.com>
2024-12-01 18:19:00 +0100

    add playwright

--- src/main.ts
+++ src/main.ts
@@ -245,7 +245,6 @@ export class GeoGrid {
         }
 
         const bounds = this.map.getBounds();
-        // @ts-expect-error
         const isGlobeProjection = this.map.getStyle().projection?.type === 'globe';
         let currentLattitude = Math.ceil(bounds.getSouth() / densityInDegrees) * densityInDegrees;
         for (; currentLattitude < bounds.getNorth(); currentLattitude += densityInDegrees) {
---
+++ tests/setup.ts
@@ -0,0 +1,5 @@
+import { test as playwrightTest, mergeTests, expect as playwrightExpect, mergeExpects } from '@playwright/test';
+import { test as mapGrabTest, expect as mapGrabExpect } from '@mapgrab/playwright';
+
+export const test = mergeTests(playwrightTest, mapGrabTest);
+export const expect = mergeExpects(playwrightExpect, mapGrabExpect);
